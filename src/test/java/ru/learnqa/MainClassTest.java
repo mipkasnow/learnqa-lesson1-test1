6ac2d3e10aa0e734d3bf37724f4a75f7ed072271
Mikhail <steamer08@mail.ru>
2022-12-02 20:26:22 +0300

    test 3 commit corrections

--- src/test/java/ru/learnqa/MainClassTest.java
+++ src/test/java/ru/learnqa/MainClassTest.java
@@ -2,6 +2,8 @@ package ru.learnqa;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.regex.Pattern;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MainClassTest {
@@ -41,5 +43,27 @@ public class MainClassTest {
         else {
             fail("Строка \"" + classString + "\" не содержит в себе подстроку \"" + hello + "\"");
         }
+    }
+
+    @Test
+    public void testGetClassString2(){
+        String classString = this.mainClass.getClassString();
+        String hello1 = "Hello";
+        String hello2 = "hello";
+        boolean isContains = classString.contains(hello1) || classString.contains(hello2);
+
+        assertTrue(isContains, "Строка \"" + classString + "\" не содержит в себе подстроку \"" + hello1 + "\" или \"" + hello2  + "\"");
+    }
+
+    @Test
+    public void testGetClassString3(){
+        String classString = this.mainClass.getClassString();
+        boolean isPattern = Pattern.compile("[Hh]ello, world").matcher(classString).matches();
+        // это используем, если нам важно проверить, что есть подстрока Hello или hello, и сама фраза Hello, World
+
+        boolean isPattern2 = Pattern.compile("[\\s\\S]*[Hh]ello[\\s\\S]*").matcher(classString).matches();
+        // это используем, если нам важно проверить, что есть подстрока Hello или hello, а остальные символы не важны
+
+        assertTrue(isPattern2, "Строка \"" + classString + "\" не содержит в себе подстроку \"Hello\" или \"hello\"");
     }
 }
